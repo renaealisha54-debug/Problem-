b8ee4a0a8b6b8d8819410ffeee9858806b0c3133
renaealisha54-debug <renaealisha54@gmail.com>
2026-05-11 13:23:33 -0400

    Create app.config.js

---
+++ Problem--main/app.config.js
@@ -0,0 +1,15 @@
+export default {
+  name: "Problem",
+  slug: "problem",
+  version: "1.0.0",
+  android: {
+    package: "com.renaealisha.problem",
+    permissions: [
+      "SYSTEM_ALERT_WINDOW",
+      "FOREGROUND_SERVICE",
+      "BIND_NOTIFICATION_LISTENER_SERVICE",
+      "POST_NOTIFICATIONS",
+      "READ_PHONE_STATE"
+    ]
+  }
+};
